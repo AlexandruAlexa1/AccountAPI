4f7544156935efae0f183197ea1596f50d83b01e
Alexandru Alexa <x.alx@yahoo.com>
2022-08-14 03:19:01 -0800

    Create Account Entity

---
+++ src/main/java/com/aa/account/Account.java
@@ -0,0 +1,57 @@
+package com.aa.account;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "accounts")
+public class Account {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(length = 20, nullable = false, unique = true)
+	private String accountNumber;
+	
+	private float balance;
+	
+	public Account() {
+		
+	}
+
+	public Account(String accountNumber, float balance) {
+		this.accountNumber = accountNumber;
+		this.balance = balance;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public float getBalance() {
+		return balance;
+	}
+
+	public void setBalance(float balance) {
+		this.balance = balance;
+	}
+	
+	
+}
