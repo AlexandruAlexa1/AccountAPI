11d1212cdc401e9736cce1dd30c52d747533c433
Alexandru Alexa <x.alx@yahoo.com>
2022-08-14 23:28:31 -0800

    update listAll() add ResponseEntity

--- src/main/java/com/aa/account/AccountRestController.java
+++ src/main/java/com/aa/account/AccountRestController.java
@@ -3,6 +3,8 @@ package com.aa.account;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +17,13 @@ public class AccountRestController {
 	private AccountService service;
 	
 	@GetMapping
-	public List<Account> listAll() {
-		return service.listAll();
+	public ResponseEntity<List<Account>> listAll() {
+		List<Account> listAccounts = service.listAll();
+		
+		if (listAccounts.isEmpty()) {
+			return ResponseEntity.noContent().build();
+		}
+		
+		return new ResponseEntity<>(listAccounts, HttpStatus.OK);
 	}
 }
