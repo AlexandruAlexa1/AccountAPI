1be3bb38fe4d9213816504c4ff2bf6ee23ff1383
Alexandru Alexa <x.alx@yahoo.com>
2022-08-14 23:33:27 -0800

    Add getOne() method in RestController

--- src/main/java/com/aa/account/AccountRestController.java
+++ src/main/java/com/aa/account/AccountRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,4 +27,10 @@ public class AccountRestController {
 		
 		return new ResponseEntity<>(listAccounts, HttpStatus.OK);
 	}
+	
+	@GetMapping("/{id}")
+	public Account getOne(@PathVariable("id") Integer id) {
+		return service.get(id);
+	}
+	
 }
